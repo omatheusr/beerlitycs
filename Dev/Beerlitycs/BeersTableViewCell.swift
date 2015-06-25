c3d290eabef515c2b4f8821920593289b57e9141
Matheus Frozzi Alberton <matheusfrozzi@MacBook-de-Frozzi.local>
2015-06-25 13:27:43 -0300

    Estrutura do StoryBoard

---
+++ Dev/Beerlitycs/BeersTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BeersTableViewCell.swift
+//  Beerlitycs
+//
+//  Created by Matheus Frozzi Alberton on 25/06/15.
+//  Copyright (c) 2015 BEPiD. All rights reserved.
+//
+
+import UIKit
+
+class BeersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Dev/Beerlitycs/BeersViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BeersViewController.swift
+//  Beerlitycs
+//
+//  Created by Matheus Frozzi Alberton on 25/06/15.
+//  Copyright (c) 2015 BEPiD. All rights reserved.
+//
+
+import UIKit
+
+class BeersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
